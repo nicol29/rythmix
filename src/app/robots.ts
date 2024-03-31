8ca3260082db891cb82099b43e10226ff4439190
nicol29 <nicolascondrea@gmail.com>
2024-03-31 05:48:45 +0100

    Add robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,20 @@
-// import { MetadataRoute } from 'next'
+import { MetadataRoute } from 'next';
  
-// export default function robots(): MetadataRoute.Robots {
-//   return {
-//     rules: {
-//       userAgent: '*',
-//       allow: '/',
-//       disallow: '/private/',
-//     },
-//     sitemap: 'https://acme.com/sitemap.xml',
-//   }
-// }
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: [
+        '/settings/notifications/', 
+        '/settings/profile/', 
+        '/settings/payouts/',
+        '/order-confirmation/',
+        '/track/edit/',
+        '/track/upload/',
+        '/tracks/',
+        '/tracks/drafts/'
+      ],
+    },
+  }
+}
